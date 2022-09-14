6823d272674689dc4d24277af677ebc12270b3db
PC6-01\PC1-17 <sistemas@artesyletras.com.co>
2022-09-13 21:39:13 -0500

    Cohort2 (Week 1) final

--- logic/logic.js
+++ logic/logic.js
@@ -1,5 +1,25 @@
 
 function Validar() {
-    let checkBoxP=document.getElementById("p").checked;
-    console.log(checkBoxP);
-}
+        let checkBoxP=document.getElementById("p").checked;
+        console.log(checkBoxP);
+        let checkBoxQ=document.getElementById("q").checked;
+        console.log(checkBoxQ);
+        let resultado="p AND q es: " + (checkBoxP&&checkBoxQ)+"<br>";
+        resultado=resultado+"q or p es: " + (checkBoxP||checkBoxQ)+"<br>";
+        resultado=resultado+"La negación or p es: " + (!checkBoxP)+"<br>";
+        resultado=resultado+"La negación or q es: " + (!checkBoxQ)+"<br>";
+        document.getElementById("message").innerHTML=resultado;
+    }
+
+    
+    // function Validar() {
+        //     if(Validar) {
+            //         let checkBoxP=document.getElementById("p").checked;
+            //         console.log(checkBoxP);
+            //     }
+            //     else if(Validar) {
+                //         let checkBoxQ=document.getElementById("q").checked;
+                //         console.log(checkBoxQ);
+                //     }
+                // }
+                
