ecaccac71b428cc89f75abbed59f975a5e544747
PC6-01\PC1-17 <sistemas@artesyletras.com.co>
2022-09-20 21:18:21 -0500

    England (Html,css,json)

--- cohort_2/cohort2.js
+++ cohort_2/cohort2.js
@@ -44,3 +44,127 @@ if(age > 18) {
 else {
     alert("Eres menor de edad")
 }
+
+// arrays
+
+let colores = ["Azul", "Amarillo", "Negro", "Blanco"]
+
+colores[2] //Negro
+
+let auto = {
+    "brand": "BMW",
+    "tyres": 4,
+    "circulation": true,
+    "owner": null
+}
+
+auto.tyres // 4
+
+let vehiculs= [
+    {
+        "brand":"audi",
+    },
+    {
+        "brand":"chevrolet",
+    }
+]
+
+let animals= [
+    {
+        "animal":"bird"
+    },
+    {
+        "animal":"tiger"
+    },
+    {
+        "animal":"fox"
+    },
+    {
+        "animal":"whale"
+    },
+    {
+        "animal":"elephant"
+    }
+]
+
+let countries= [
+    {
+        "name": "Colombia" ,
+        "capital": "Bogotá" ,
+        "kilometers": 1139000,
+        "language": "Español",
+        "PIB": 271.3
+    },
+    {
+        "name": "Canada" ,
+        "capital": "Ottawa" ,
+        "kilometers": 9985,
+        "language": "Ingles" ,
+        "PIB": 1682.943,
+    }
+]
+
+
+let singers= [
+    {
+        "name": "Ed Sheeran",
+        "nacionality": "Halifax, Reino Unido",
+        "musical_genre": "Pop",
+        "songs": [
+            {
+                "name": "Photograph",
+                "views": "15704439"
+            },
+            {
+                "name": "Thinking Out Loud",
+                "views": "3496943672"
+            },
+            {
+                "name": "Shape of You",
+                "views": "5805662660"
+            },
+        ]
+    },
+    {
+        "name": "Donald Glover ",
+        "nacionality": "American",
+        "musical_genre": "hip-hop/rap",
+        "songs": [
+            {
+                "name": "Redbone",
+                "views": "577327853"
+            },
+            {
+                "name": "This Is America",
+                "views": "852552728"
+            },
+            {
+                "name": "3005",
+                "views": "223163311"
+            },
+        ]
+    },
+    {
+        "name": "Rihana",
+        "nacionality": "Barbadense",
+        "musical_genre": "Electronic dance",
+        "songs": [
+            {
+                "name": "This Is What You Came For",
+                "views": "2595159842 "
+            },
+            {
+                "name": "Diamonds",
+                "views": "2000463062"
+            },
+            {
+                "name": "Man Down",
+                "views": "875346598"
+            }
+        ]
+    }
+]
+
+console.log(singers)
+
+
